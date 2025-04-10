7aab7f682c15b5853b468988e3bbd24115bbab29
Albin Bachler Jansson <albin.bachler.jansson@gmail.com>
2025-04-10 15:07:47 +0200

    Delete src directory
    asd

--- src/build/demos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rosdev/ROS2/src/demos/src/demos
-# Build directory: /home/rosdev/ROS2/src/build/demos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
