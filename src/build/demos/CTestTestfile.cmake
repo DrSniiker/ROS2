86521bcf793b93d895e335a3796b8e766526e332
Albin Bachler Jansson <albin.bachler.jansson@gmail.com>
2025-04-10 15:02:00 +0200

    added demos repo

---
+++ src/build/demos/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosdev/ROS2/src/demos/src/demos
+# Build directory: /home/rosdev/ROS2/src/build/demos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
